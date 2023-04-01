7a03cc0c813d0b38337658bf5400cd530b76c50d
Dxuvian08091 <dxuvian@gmail.com>
2023-04-01 21:43:13 +0545

    check

--- app.js
+++ app.js
@@ -32,6 +32,10 @@ app.use(express.json());
 
 app.use("/video", express.static(path.join(__dirname, "video")));
 
+app.use((req, res, next) => {
+  res.status(200).json(process.env.VITE_URL);
+});
+
 app.use((err, req, res, next) => {
   if (!(err instanceof Error)) {
     err = new Error(err);
